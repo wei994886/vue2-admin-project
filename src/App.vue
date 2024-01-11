0ce89257c29ca547e3910278d0ee3994e10223fd
weimaosheng <923269113@qq.com>
2024-01-11 16:12:28 +0800

    ✨ feat：修改配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-
-#app {
-  width: 100%;
-  height: 100%;
-}
-
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router';
 import store from './store';
+
+import './styles/main.scss'
  
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
-  el: '#app',
   render: h => h(App)
 }).$mount('#app')
